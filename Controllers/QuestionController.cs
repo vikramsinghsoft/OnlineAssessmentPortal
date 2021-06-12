a1e3ad9358bd0d421668f889a7b19419bb968123
Vikram Bahadur Singh <78192923+vikramsinghsoft@users.noreply.github.com>
2021-06-12 16:12:08 +0530

    Add files via upload

---
+++ Controllers/QuestionController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineTestPortal.Models;
+
+namespace OnlineTestPortal.Controllers
+{
+    public class QuestionController : Controller
+    {
+        // GET: Question
+        public ActionResult Index()
+        {
+            MYDBEntities db = new MYDBEntities();
+            var questions = db.Questions.Where(x=>x.IsAttempt==false);
+            return View(questions);
+        }
+
+        [HttpPost]
+        public ActionResult Save(FormCollection form)
+        {
+            foreach (string item in form)
+            {
+                var val = form[item];
+                if (!string.IsNullOrEmpty(val))
+                {
+                    MYDBEntities db = new MYDBEntities();
+                    var ans = db.Answers.Find(Convert.ToInt32(val));
+                    ans.userAns = ans.Answer1;
+                    db.SaveChanges();
+
+                    var id = ans.QuestionId;
+                    var question = db.Questions.Find(id);
+
+                    question.IsAttempt = true;
+                    db.SaveChanges();
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/QuestionanswerVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTestPortal.Models
+{
+    public class QuestionanswerVM
+    {
+        public Question Question { get; set; }
+        public List<Answer> Answers { get; set; }
+    }
+}
